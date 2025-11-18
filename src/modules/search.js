5b0f8c7855cde58272dc588466dd1d348278e8f2
Oleksandr Vykhor <sashavihor67@gmail.com>
2025-11-18 20:19:30 +0100

    add search

---
+++ src/modules/search.js
@@ -0,0 +1,19 @@
+import getData from "./getData.js";
+import renderGoods from "./renderGoods.js";
+import { searchFilter } from "./filters.js";
+
+const search = () => {
+  const searchInput = document.querySelector(".search-wrapper_input");
+
+  searchInput.addEventListener("input", (event) => {
+    const value = event.target.value;
+
+    getData().then((data) => {
+      renderGoods(searchFilter(data, value));
+    });
+  });
+};
+
+console.log("hello");
+
+export default search;
