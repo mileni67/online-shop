be1859ac4a64c820c6a1e867211f9cc93935dbdd
Oleksandr Vykhor <sashavihor67@gmail.com>
2025-11-18 20:18:57 +0100

    deleted console.log

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,9 +1,6 @@
 const getData = () => {
-  console.log("Fetching data...");
   return fetch(
     "https://shop-6bb26-default-rtdb.firebaseio.com/goods.json"
   ).then((res) => res.json());
 };
-
-console.log("getData.js loaded");
 export default getData;
