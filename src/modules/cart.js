d3058afadcac1e0e665a05ab61a684a0ae303daa
Oleksandr Vykhor <sashavihor67@gmail.com>
2025-11-19 21:26:49 +0100

    add new func

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,10 +1,25 @@
+import postData from "./postData.js";
+import renderCart from "./renderCart.js";
+
 const cart = () => {
   const cartBtn = document.getElementById("cart");
   const cartModal = document.querySelector(".cart");
   const cartCloseBtn = cartModal.querySelector(".cart-close");
+  const cartTotal = cartModal.querySelector(".cart-total > span");
+  const goodsWrapper = document.querySelector(".goods");
+  const cartWrapper = document.querySelector(".cart-wrapper");
+  const cartSendBtn = document.querySelector(".cart-confirm");
 
   const openCart = () => {
+    const cart = localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
     cartModal.style.display = "flex";
+
+    renderCart(cart);
+    cartTotal.textContent = cart.reduce((sum, goodItem) => {
+      return sum + goodItem.price;
+    }, 0);
   };
 
   const closeCart = () => {
@@ -13,6 +28,59 @@ const cart = () => {
 
   cartBtn.addEventListener("click", openCart);
   cartCloseBtn.addEventListener("click", closeCart);
+
+  goodsWrapper.addEventListener("click", (event) => {
+    if (event.target.classList.contains("btn-primary")) {
+      const card = event.target.closest(".card");
+      const key = card.dataset.key;
+      const goods = JSON.parse(localStorage.getItem("goods"));
+      const cart = localStorage.getItem("cart")
+        ? JSON.parse(localStorage.getItem("cart"))
+        : [];
+      const goodItem = goods.find((item) => item.id === key);
+      if (!goodItem) return;
+
+      cart.push(goodItem);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  });
+
+  cartWrapper.addEventListener("click", (event) => {
+    if (event.target.classList.contains("btn-primary")) {
+      const cart = localStorage.getItem("cart")
+        ? JSON.parse(localStorage.getItem("cart"))
+        : [];
+
+      const card = event.target.closest(".card");
+      const key = card.dataset.key;
+      const index = cart.findIndex((item) => {
+        return item.id === +key;
+      });
+
+      cart.splice(index, 1);
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+
+      renderCart(cart);
+      cartTotal.textContent = cart.reduce((sum, goodItem) => {
+        return sum + goodItem.price;
+      }, 0);
+    }
+  });
+
+  cartSendBtn.addEventListener("click", () => {
+    const cart = localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+
+    postData(cart).then(() => {
+      localStorage.removeItem("cart");
+
+      renderCart([]);
+
+      cartTotal.textContent = 0;
+    });
+  });
 };
 
 export default cart;
