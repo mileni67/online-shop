e67d84311b520f954e1775aabd9605d69f4cd69f
Oleksandr Vykhor <sashavihor67@gmail.com>
2025-11-19 21:26:14 +0100

    add filter

---
+++ src/modules/filter.js
@@ -0,0 +1,54 @@
+import { priceFilter, hotSaleFilter } from "./filters.js";
+import getData from "./getData.js";
+import renderGoods from "./renderGoods.js";
+
+const filter = () => {
+  const minInput = document.getElementById("min");
+  const maxInput = document.getElementById("max");
+  const checkboxInput = document.getElementById("discount-checkbox");
+  const checkboxSpan = document.querySelector(".filter-check_checkmark");
+
+  minInput.addEventListener("input", () => {
+    getData().then((data) => {
+      renderGoods(
+        priceFilter(
+          hotSaleFilter(data, checkboxInput.checked),
+          minInput.value,
+          maxInput.value
+        )
+      );
+    });
+  });
+
+  maxInput.addEventListener("input", () => {
+    getData().then((data) => {
+      renderGoods(
+        priceFilter(
+          hotSaleFilter(data, checkboxInput.checked),
+          minInput.value,
+          maxInput.value
+        )
+      );
+    });
+  });
+
+  checkboxInput.addEventListener("change", () => {
+    if (checkboxInput.checked) {
+      checkboxSpan.classList.add("checked");
+    } else {
+      checkboxSpan.classList.remove("checked");
+    }
+
+    getData().then((data) => {
+      renderGoods(
+        priceFilter(
+          hotSaleFilter(data, checkboxInput.checked),
+          minInput.value,
+          maxInput.value
+        )
+      );
+    });
+  });
+};
+
+export default filter;
