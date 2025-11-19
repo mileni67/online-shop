0c54764adc045916616053ff65e142c5ed130407
Oleksandr Vykhor <sashavihor67@gmail.com>
2025-11-19 21:26:29 +0100

    add import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import cart from "./modules/cart.js";
 import catalog from "./modules/catalog.js";
+import filter from "./modules/filter.js";
+import getData from "./modules/getData.js";
 import load from "./modules/load.js";
+import renderGoods from "./modules/renderGoods.js";
 import search from "./modules/search.js";
 
 cart();
 load();
 search();
 catalog();
+filter();
+
+getData().then((data) => {
+  localStorage.setItem("goods", JSON.stringify(data));
+  renderGoods(data);
+});
